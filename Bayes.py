95d22738543fbf9c9a4743e1cf7330e5a019ee03
shixi_enyang <shixi_enyang@staff.weibo.com>
2017-06-27 14:29:29 +0800

    modify Bayes.py

--- Bayes.py
+++ Bayes.py
@@ -33,10 +33,11 @@ def trainNB0(trainmatrix , trainCategory):
 	numTrainsDocs = len(trainmatrix)
 	numWords = len(trainmatrix[0])
 	pAbusive = sum(trainCategory) / float(numTrainsDocs)
-	p0Num = zeros(numWords)
-	p1Num = zeros(numWords)
-	p0Denom = 0.0
-	p1Denom = 0.0
+	# 防止出现概率为0的情况
+	p0Num = ones(numWords)
+	p1Num = ones(numWords)
+	p0Denom = 2.0
+	p1Denom = 2.0
 	for i in range(numTrainsDocs):
 		if trainCategory[i] == 1:
 			p1Num += trainmatrix[i]
@@ -44,6 +45,26 @@ def trainNB0(trainmatrix , trainCategory):
 		else:
 			p0Num += trainmatrix[i]
 			p0Denom += sum(trainmatrix[i])
-	p1Vec = p1Num / p1Denom
-	p0Vec = p0Num / p0Denom
+	p1Vec = log(p1Num/p1Denom)
+	p0Vec = log(p0Num/p0Denom)
 	return p0Vec , p1Vec , pAbusive
+
+#朴素的贝叶斯分类函数
+def classifyNB(vec2Classify , p0Vec , p1Vec , pClass1):
+	p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+	p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+	if p1 > p0:
+		return 1
+	else:
+		return 0
+
+def testingNB():
+	listOPosts , listClasses = loadDataSet()
+	myVocabList = createVocabList(listOPosts)
+	trainMat = []
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList , postinDoc))
+	p0V , p1V , pAb = trainNB0(array(trainMat) , array(listClasses))
+	testEntry = ['love' , 'my' , 'dalmation']
+	thisDoc = array(setOfWords2Vec(myVocabList , testEntry))
+	print testEntry , 'classified as: ' , classifyNB(thisDoc , p0V , p1V , pAb)
