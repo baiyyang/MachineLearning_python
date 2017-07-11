260c355b897784549dae17bd9d3567c6a345316b
shixi_enyang <shixi_enyang@staff.weibo.com>
2017-07-11 15:03:15 +0800

    update CART

--- regression/CART.py
+++ regression/CART.py
@@ -47,6 +47,7 @@ def regErr(dataSet):
 	return var(dataSet[: , -1]) * shape(dataSet)[0]
 
 #ops分别记录误差的阈值和分类的最小的误差值
+#返回值为列信息和列中最好的值
 def chooseBestSplit(dataSet , leafType=regLeaf , errType=regErr , ops=(1,4)):
 	tolS = ops[0]
 	tolN = ops[1]
@@ -109,3 +110,57 @@ def prune(tree , testData):
 			return tree
 	else:
 		return tree 
+
+#模型树叶节点的生成函数
+def linearSolve(dataSet):
+	m , n = shape(dataSet)
+	X = mat(ones((m , n)))
+	Y = mat(ones((m , 1)))
+	X[: , 1 : n] = dataSet[: , 0:n-1]
+	Y = dataSet[: , -1]
+	xTx = X.T * X
+	if linalg.det(xTx) == 0.0:
+		raise NameError('This matrix is singular, cannot do inverse, \n \
+			try increasing the second value of ops')
+	ws = xTx.I * (X.T * Y)
+	return ws , X , Y
+
+def modelLeaf(dataSet):
+	ws , X , Y = linearSolve(dataSet)
+	return ws
+
+def modelErr(dataSet):
+	ws , X , Y = linearSolve(dataSet)
+	yHat = X * ws
+	return sum(power(Y - yHat , 2))
+
+#使用树回归进行预测
+def regTreeEval(model , inDat):
+	return float(model)
+
+def modelTreeEval(model , inDat):
+	n = shape(inDat)[1]
+	X = mat(ones((1 , n+1)))
+	X[: , 1:n+1] = inDat
+	return float(X * model)
+
+def treeForeCast(tree , inData , modelEval = regTreeEval):
+	if not isTree(tree):
+		return modelEval(tree  ,inData)
+	if inData[tree['spInd']] > tree['spVal']:
+		if isTree(tree['left']):
+			return treeForeCast(tree['left'] , inData , modelEval)
+		else:
+			return modelEval(tree['left'] , inData)
+	else:
+		if isTree(tree['right']):
+			return treeForeCast(tree['right'] , inData , modelEval)
+		else:
+			return modelEval(tree['right'] , inData)
+
+def createForeCast(tree , testData , modelEval=regTreeEval):
+	m = len(testData)
+	yHat = mat(zeros((m ,1)))
+	for i in range(m):
+		yHat[i , 0] = treeForeCast(tree , mat(testData[i]) , modelEval)
+	return yHat
